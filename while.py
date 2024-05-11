b0ac841abf23303b75b930af4ab7555074821a08
PaulinaCampusano <168911609+PaulinaCampusano@users.noreply.github.com>
2024-05-10 20:30:35 -0400

    Add files via upload

---
+++ while.py
@@ -0,0 +1,11 @@
+#actividadwhile
+
+a = 5
+"""while a > 0:
+    print(f"El valor de a es: {a}")
+    a = int(input("ingrese un valor : "))"""
+    
+while a != 6:
+    print(f"el valor de a es : {a}")
+    a = int(input("ingrese un valor : "))
+     
